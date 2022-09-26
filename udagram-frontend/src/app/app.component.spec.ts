529a0e9c1d2c75bf7002a6649fd4d76ffd06d881
mehdi ben hariz <mehdi@pop-os.localdomain>
2022-09-25 20:26:08 -0500

    fixed front issue

--- udagram-frontend/src/app/app.component.spec.ts
+++ udagram-frontend/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 
 import { Platform } from '@ionic/angular';
