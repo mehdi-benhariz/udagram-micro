499bac1f0a6e9d4c7a728a3a730f4b914c17e50c
mehdi ben hariz <mehdi@pop-os.localdomain>
2022-09-25 20:16:53 -0500

    fixed front issue

--- udagram-frontend/src/app/app.module.ts
+++ udagram-frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -14,6 +14,9 @@ import { AuthModule } from './auth/auth.module';
 import { ApiService } from './api/api.service';
 
 @NgModule({
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   declarations: [
     AppComponent,
     MenubarComponent
