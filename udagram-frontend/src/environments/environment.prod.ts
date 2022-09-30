05654756154dd7b9d7836da5be75809108888ab4
mehdi ben hariz <mehdi@pop-os.localdomain>
2022-09-30 11:45:35 -0500

    final touches

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://abd6bf79296544b87a403128b896b3aa-1812786950.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
